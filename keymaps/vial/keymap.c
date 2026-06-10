c64785691c2a1acd585dc3a29bb163d9214b9318
Eduardo <eduardo.costa.xyz@gmail.com>
2026-06-10 17:45:51 -0300

    feat: add unicode accents support

--- keymaps/vial/keymap.c
+++ keymaps/vial/keymap.c
@@ -1,14 +1,12 @@
-// Copyright 2023 QMK
-// SPDX-License-Identifier: GPL-2.0-or-later
-
 #include QMK_KEYBOARD_H
+#include "user_unicode.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_NO,   KC_B,    KC_L,    KC_D,    KC_C,    KC_V,       KC_J,    KC_Y,    KC_O,    KC_U,    KC_NO,   KC_NO,
+        KC_NO,   KC_N,    KC_R,    KC_T,    KC_S,    KC_G,       KC_P,    KC_H,    KC_A,    KC_E,    KC_I,    KC_NO,
+        KC_NO,   KC_X,    KC_Q,    KC_M,    KC_W,    KC_Z,       KC_K,    KC_F,    KC_NO,   KC_NO,   KC_NO,   KC_NO,
                                    KC_NO,   KC_NO,   KC_NO,      KC_NO,   KC_NO,   KC_NO
     ),
 
@@ -77,3 +75,31 @@ const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
         'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R',
                        'L', 'L', 'L',  'R', 'R', 'R'
     );
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (record->event.pressed) {
+        bool shifted = (get_mods() & MOD_MASK_SHIFT);
+
+        switch (keycode) {
+            case UNI_LNX:  set_unicode_input_mode(UNICODE_MODE_LINUX); return false;
+            case UNI_WIN:  set_unicode_input_mode(UNICODE_MODE_WINCOMPOSE); return false;
+            case UNI_MAC:  set_unicode_input_mode(UNICODE_MODE_MACOS); return false;
+            case UNI_NEXT: unicode_input_mode_step(); return false;
+            case UNI_PREV: unicode_input_mode_step_reverse(); return false;
+
+            case A_ACUT: register_unicode(unicodemap_get_code_point(shifted ? A_ACUT_U : A_ACUT_L)); return false;
+            case A_CIRC: register_unicode(unicodemap_get_code_point(shifted ? A_CIRC_U : A_CIRC_L)); return false;
+            case A_TILD: register_unicode(unicodemap_get_code_point(shifted ? A_TILD_U : A_TILD_L)); return false;
+            case A_GRAV: register_unicode(unicodemap_get_code_point(shifted ? A_GRAV_U : A_GRAV_L)); return false;
+            case E_ACUT: register_unicode(unicodemap_get_code_point(shifted ? E_ACUT_U : E_ACUT_L)); return false;
+            case E_CIRC: register_unicode(unicodemap_get_code_point(shifted ? E_CIRC_U : E_CIRC_L)); return false;
+            case I_ACUT: register_unicode(unicodemap_get_code_point(shifted ? I_ACUT_U : I_ACUT_L)); return false;
+            case O_ACUT: register_unicode(unicodemap_get_code_point(shifted ? O_ACUT_U : O_ACUT_L)); return false;
+            case O_CIRC: register_unicode(unicodemap_get_code_point(shifted ? O_CIRC_U : O_CIRC_L)); return false;
+            case O_TILD: register_unicode(unicodemap_get_code_point(shifted ? O_TILD_U : O_TILD_L)); return false;
+            case U_ACUT: register_unicode(unicodemap_get_code_point(shifted ? U_ACUT_U : U_ACUT_L)); return false;
+            case C_CEDL: register_unicode(unicodemap_get_code_point(shifted ? C_CEDL_U : C_CEDL_L)); return false;
+        }
+    }
+    return true;
+}
